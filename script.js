5a704b3f2b30b2f974dc96ad5fb116532785685e
SarutPhanna <145453646+SarutPhanna@users.noreply.github.com>
2023-12-15 23:08:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function todo() {
   if (inputTitle.value === "") {
     alert("You should enter text.");
     return;
-  }
+  };
 
   const todo = document.createElement("div");
   todo.classList.add("todo");
@@ -21,20 +21,20 @@ function todo() {
   deleteButton.innerHTML = "Delete";
   deleteButton.addEventListener("click", (e) => {
     e.target.parentNode.remove();
-  });   
+  });
 
-  const completed = document.createElement('button');
-  completed.type = 'button';
-  completed.classList.add('completed');
-  completed.innerHTML = 'Completed';
-  completed.addEventListener('click',(e)=>{
+  const completed = document.createElement("button");
+  completed.type = "button";
+  completed.classList.add("completed");
+  completed.innerHTML = "Completed";
+  completed.addEventListener("click", (e) => {
     const parentTodo = e.target.parentNode;
-    parentTodo.classList.toggle('completed-to-do-list');
+    parentTodo.classList.toggle("completed-to-do-list");
   });
 
   todo.append(list, completed, deleteButton);
   mainTodolist.append(todo);
-}
+};
 
 runButtonTodo.addEventListener("click", todo);
 runButtonTodo.addEventListener("click", () => {
